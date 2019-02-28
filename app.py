d8503e1811adbbe2d9ee4fc25aa9625216d72266
Pius Dan <npiusdan@gmail.com>
2019-02-28 22:02:49 +0300

    Add support for dotenv files

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
 
+from dotenv import load_dotenv
+
 from flask import Flask
 from flask import render_template, flash, redirect, url_for
 from flask_mail import Mail, Message
 from flask_wtf import FlaskForm as Form
-from wtforms import TextAreaField, validators
+from wtforms import StringField, TextAreaField, validators
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -31,7 +35,7 @@ class ContactForm(Form):
     name = StringField('Name', [validators.Length(min=4, max=25), validators.DataRequired()], description='Your Name')
     email = StringField('Email Address', [validators.Length(min=6, max=35), validators.DataRequired()])
     phone = StringField('Phone Number', [validators.DataRequired()])
-    message = StringField('Message', [validators.Length(min=10, max=200), validators.DataRequired()])
+    message = TextAreaField('Message', [validators.Length(min=10, max=200), validators.DataRequired()])
 
 
 def send_mail(message, name, phone, email):
@@ -47,7 +51,11 @@ def send_mail(message, name, phone, email):
 def index():
     form = ContactForm()
     if form.validate_on_submit():
-        send_mail(email=form.email.data, name=form.name.data, message=form.message.data, phone=form.phone.data)
+        try:
+            send_mail(email=form.email.data, name=form.name.data, message=form.message.data, phone=form.phone.data)
+        except Exception as exc:
+            app.logger.error(f"Encountered error sending mail {exc}")
+
         flash('Message sent. Pius will be notified soon!')
         return redirect(url_for('.index'))
     return render_template("index.html", form=form)
